05a95ee6111e94d6904bb6f376246348fb854a1b
fbamuse <fukuhara.masanori@gmail.com>
2021-06-12 22:06:12 +0900

    main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -25,19 +25,19 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 # TODO: replace the all-zero GUID with your instrumentation key.
 logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey={guid}')
+    connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
 )
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
   enable_standard_metrics=True,
-  connection_string='InstrumentationKey={guid}')
+  connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
 
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey={guid}'),
+        connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/'),
     sampler=ProbabilitySampler(1.0),
 )
 app = Flask(__name__)
@@ -45,7 +45,7 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string="InstrumentationKey={guid}"),
+    exporter=AzureExporter(connection_string="InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/"),
     sampler=ProbabilitySampler(rate=1.0),
 )
 
