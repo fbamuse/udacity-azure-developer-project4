f5c4ccf3f1c868737d07b1ac30caf3f51707c1cb
fbamuse <fukuhara.masanori@gmail.com>
2021-06-13 15:37:35 +0900

    port 5000

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,45 +9,45 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
-# from opencensus.ext.azure.log_exporter import AzureLogHandler
-# from opencensus.ext.azure import metrics_exporter
-# from opencensus.stats import aggregation as aggregation_module
-# from opencensus.stats import measure as measure_module
-# from opencensus.stats import stats as stats_module
-# from opencensus.stats import view as view_module
-# from opencensus.tags import tag_map as tag_map_module
-# from opencensus.ext.azure.trace_exporter import AzureExporter
-# from opencensus.trace.samplers import ProbabilitySampler
-# from opencensus.trace.tracer import Tracer
-# from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+ from opencensus.ext.azure.log_exporter import AzureLogHandler
+ from opencensus.ext.azure import metrics_exporter
+ from opencensus.stats import aggregation as aggregation_module
+ from opencensus.stats import measure as measure_module
+ from opencensus.stats import stats as stats_module
+ from opencensus.stats import view as view_module
+ from opencensus.tags import tag_map as tag_map_module
+ from opencensus.ext.azure.trace_exporter import AzureExporter
+ from opencensus.trace.samplers import ProbabilitySampler
+ from opencensus.trace.tracer import Tracer
+ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 #
 # Logging
 logger = logging.getLogger(__name__)
 # TODO: replace the all-zero GUID with your instrumentation key.
-#logger.addHandler(AzureLogHandler(
-#    connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
-#)
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
+)
 
 # Metrics
-#exporter = metrics_exporter.new_metrics_exporter(
-#  enable_standard_metrics=True,
-#  connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
 
 
 # Tracing
-#tracer = Tracer(
-#    exporter=AzureExporter(
-#        connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/'),
-#    sampler=ProbabilitySampler(1.0),
-#)
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/'),
+    sampler=ProbabilitySampler(1.0),
+)
 app = Flask(__name__)
 
 # Requests
-#middleware = FlaskMiddleware(
-#    app,
-#    exporter=AzureExporter(connection_string="InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/"),
-#    sampler=ProbabilitySampler(rate=1.0),
-#)
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=InstrumentationKey=acd254b2-d6fc-4337-b024-0dfc99f09841;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -85,11 +85,11 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-#        tracer.span(name="cat vote")
+        tracer.span(name="cat vote")
         # TODO: use tracer object to trace cat vote
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-#        racer.span(name="dog vote")
+        racer.span(name="dog vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -103,11 +103,11 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-#            logger.warning('action', extra=properties)
+            logger.warning('action', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-#            logger.warning('action', extra=properties)
+            logger.warning('action', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
