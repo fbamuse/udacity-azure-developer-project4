6e229a49efd3e1198eb646ea6ae4f94a1b83d8b0
fbamuse <fukuhara.masanori@gmail.com>
2021-06-13 19:31:01 +0900

    port 5000

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -85,7 +85,7 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-       
+        tracer.span(name="cat")
         #with tracer.span(name='span1'):
         #    print("cat vote")
         # TODO: use tracer object to trace cat vote
@@ -93,7 +93,7 @@ def index():
         # TODO: use tracer object to trace dog vote
         #with tracer.span(name='span1'):
         #    print("dog vote")
-        tracer.span(key="dog")
+        tracer.span(name="dog")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
