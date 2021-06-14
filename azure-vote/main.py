f312e6f65edc9c025540d237a7c8f98979f68546
fbamuse <fukuhara.masanori@gmail.com>
2021-06-14 21:51:00 +0900

    ok

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -112,11 +112,11 @@ def index():
 
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            logger.warning('Voted for cats', extra=properties)
+            logger.info('Voted for cats', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.warning('Voted for dogs', extra=properties)
+            logger.info('Voted for dogs', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -125,6 +125,11 @@ def index():
             # Insert vote result into DB
             vote = request.form['vote']
             r.incr(vote,1)
+            vote0 = r.get(vote).decode('utf-8')
+            
+            # log current vote
+            properties = {'custom_dimensions': {'{}_vote'.format(vote): vote0}}
+            logger.info('new_{}_vote'.format(vote), extra=properties)
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
