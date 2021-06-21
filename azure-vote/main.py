2e337b76405b3be655dbc796702685043b84913a
fbamuse <fukuhara.masanori@gmail.com>
2021-06-21 10:54:03 +0900

    ok

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -105,15 +105,15 @@ def index():
 
 
             vote1 = r.get(button1).decode('utf-8')
-            properties = {'customDimensions': {'Cats Vote': vote1}}
+            properties = {'custom_dmensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-            logger.info('CAT', extra=properties)
+            logger.info('CAT_Vote', extra=properties)
             #logger.info('Cats')
 
             vote2 = r.get(button2).decode('utf-8')
-            properties = {'customDimensions': {'Dogs Vote': vote2}}
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-            logger.info('DOG', extra=properties)
+            logger.info('DOG_Vote', extra=properties)
             #logger.info('Dogs')
 
             # Empty table and return results
