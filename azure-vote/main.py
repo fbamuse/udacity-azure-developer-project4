7782958f0f6734a882f2b13d664ba13466ef560f
fbamuse <fukuhara.masanori@gmail.com>
2021-06-21 22:48:08 +0900

    ok

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -51,7 +51,7 @@ app = Flask(__name__)
 middleware = FlaskMiddleware(
     app,
     exporter=AzureExporter(connection_string="InstrumentationKey=c05c4480-e0b4-46aa-be27-cfdf45278e3f;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/"),
-    sampler=ProbabilitySampler(0.5),
+    sampler=ProbabilitySampler(1.0),
 )
 
 # Load configurations from environment or config file
