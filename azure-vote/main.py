04ad0c6ba5a9de2cc2d5ffe62c90253c22a950f6
fbamuse <fukuhara.masanori@gmail.com>
2021-06-21 20:06:52 +0900

    ok

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -35,7 +35,7 @@ logger.setLevel(logging.INFO)
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
-  enable_standard_metrics=True,
+  enable_standard_metrics=False,
   connection_string='InstrumentationKey=8c702533-74c6-42c8-b7ee-52f2ad212eca;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/')
 
 
